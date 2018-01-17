23e0c171506b1efa4da14fed916eb760c682fc47
GKonova <glori_to@abv.bg>
2018-01-17 23:53:12 +0200

    #9 some basic logic for sending emails

---
+++ app/Http/routes.php
@@ -0,0 +1,3 @@
+Route::get('sendbasicemail','MailController@basic_email');
+Route::get('sendhtmlemail','MailController@html_email');
+Route::get('sendattachmentemail','MailController@attachment_email');
